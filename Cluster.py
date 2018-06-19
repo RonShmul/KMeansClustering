b6c6dde3d4c5b48da317c33488634366dd2d015c
nogahm <p0o9i8U7>
2018-06-19 16:00:43 +0300

    Continue the GUI class

--- Cluster.py
+++ Cluster.py
@@ -1,67 +1,61 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.stats import mode
-from PIL import Image
-import plotly.plotly as py
 from sklearn.cluster import KMeans
-
+from PIL import Image
+import matplotlib.pyplot as plt
+import plotly.plotly as py
 
 class Cluster:
-    df = pd.DataFrame({})
-    kMean=pd.DataFrame({})
-    scatterPlot=None
+    scatterPlot = None
 
     def __init__(self, data_frame, num_of_clusters, num_of_runs):
         self.df = data_frame
-        self.num_of_clusters = num_of_clusters
-        self.num_of_runs = num_of_clusters
+        self.clusters = num_of_clusters
+        self.runs = num_of_runs
 
         # run the KMeans Algorithm
-        kmeans = KMeans(num_of_clusters, num_of_runs).fit(self.df)
+        k_means = KMeans(n_clusters=self.clusters, n_init=self.runs).fit(self.df)
         # add column for each country
-        updated_df = kmeans.predict(self.df)
+        updated_df = k_means.predict(self.df)
         self.df["cluster"] = updated_df
         self.scatter()
         self.horopleth_map()
 
     def scatter(self):
-        self.scatterPlot = plt.scatter(x = self.df["Social support"], y = self.df["Generosity"], c = self.df["cluster"])
-        plt.title("K-Means Clustering")
+        self.scatterPlot = plt.scatter(x=self.df["Social support"], y=self.df["Generosity"], c=self.df["cluster"])
+        plt.colorbar(self.scatterPlot)
         plt.xlabel("Social support")
         plt.ylabel("Generosity")
+        plt.title("K Means Clustering")
         plt.savefig("scatter.png")
-        plt.colorbar(self.df)
 
     def horopleth_map(self):
-        scl = [[0.0, 'rgb(242,240,247)'], [0.2, 'rgb(218,218,235)'], [0.4, 'rgb(188,189,220)'], \
+        scl = [[0.0, 'rgb(242,240,247)'], [0.2, 'rgb(218,218,235)'], [0.4, 'rgb(188,189,220)'],
                [0.6, 'rgb(158,154,200)'], [0.8, 'rgb(117,107,177)'], [1.0, 'rgb(84,39,143)']]
         data = [dict(
-            type = 'choropleth',
-            colorscale = scl,
-            autocolorscale = False,
-            locations = self.df.axes[0].tolist(),
-            z = self.df["cluster"],
-            locationmode = 'country',
-            text = self.df['country'],  # todo: not sure!! maybe also toList?
-            marker = dict(
-                line = dict(
-                    color = 'rgb(255,255,255)',
-                    width = 2
+            type='choropleth',
+            colorscale=scl,
+            autocolorscale=False,
+            locations=self.df.axes[0].tolist(),
+            z=self.df["cluster"],
+            locationmode='country',
+            text=self.df.axes[0].tolist(),
+            marker=dict(
+                line=dict(
+                    color='rgb(255,255,255)',
+                    width=2
                 )),
             colorbar=dict(
                 title="Millions USD")
         )]
 
         layout = dict(
-            title = 'K-Means Clustering Visualization',
-            geo = dict(
-                scope = 'Cluster group',
-                projection = dict(type='Mercator'),
-                showlakes = True,
-                lakecolor = 'rgb(255, 255, 255)'),
+            title='K Means Clustering Visualization',
+            geo=dict(
+                scope='Cluster group',
+                projection=dict(type='Mercator'),
+                showlakes=True,
+                lakecolor='rgb(255, 255, 255)'),
         )
         py.sign_in("ronshmul", "BFWck7n9kO0cEty7zprn")
         fig = dict(data=data, layout=layout)
-        py.iplot(fig, validate=False, filename='horopleth', auto_open=False)
+        py.plot(fig, validate=False, filename='horopleth.png', auto_open=False)
         py.image.save_as(fig, filename='horopleth.png')
--- GUI.py
+++ GUI.py
@@ -1,7 +1,10 @@
 import tkFileDialog
 import tkMessageBox
-from Tkinter import Tk, Label, Button, Entry, IntVar, END, W, E, StringVar
+from Tkinter import Tk, Label, Button, Entry, END, W, E, StringVar
+from Cluster import *
+from PreProcess import *
 import pandas as pd
+from PIL import Image, ImageTk
 
 
 class KMeansClustering:
@@ -15,6 +18,7 @@ class KMeansClustering:
         self.num_of_clusters = 0
         self.num_of_runs = 0
         self.master = master
+        self.is_processed = False
         master.title("KMeansClustering")
 
         # define path label:
@@ -32,11 +36,7 @@ class KMeansClustering:
         self.var_runs_label.set("Num of runs:")
         self.runs_label = Label(root, textvariable=self.var_runs_label)
 
-        self.total_label_text = IntVar()
-        self.total_label_text.set(self.total)
-        self.total_label = Label(master, textvariable=self.total_label_text)
-
-        vcmd_path = master.register(self.validate_path) # we have to wrap the command
+        vcmd_path = master.register(self.validate_path)
         vcmd_number = master.register(self.validate)
 
         self.path_entry = Entry(master, validate="focusout", validatecommand=(vcmd_path, '%P'))
@@ -44,8 +44,8 @@ class KMeansClustering:
         self.runs_entry = Entry(master, validate="key", validatecommand=(vcmd_number, '%P'))
 
         self.Browse_button = Button(master, text="Browse", command=lambda: self.browse())
-        self.cluster_button = Button(master, text="Cluster", command=lambda: self.update("subtract"))
-        self.preProcess_button = Button(master, text="Pre-process", command=lambda: self.update("reset"))
+        self.cluster_button = Button(master, text="Cluster", command=lambda: self.k_means())
+        self.preProcess_button = Button(master, text="Pre-process", command=lambda: self.pre_process())
 
         # LAYOUT
 
@@ -67,12 +67,13 @@ class KMeansClustering:
         if self.path:
             self.path_entry.delete(0, END)
         self.path_entry.insert(0, self.path)
-        if not self.path:
-            tkMessageBox.showinfo("K Means Clustering", "Please choose data file")
-            return
+
         if not (self.path[-5:] == ".xlsx" or self.path[-4:] == ".xls"):
             tkMessageBox.showinfo("K Means Clustering", "Please choose excel file")
             self.path_entry.delete(0, END)
+            return
+        if not self.path:
+            tkMessageBox.showinfo("K Means Clustering", "Please choose data file")
             return
         self.df = pd.read_excel(self.path)
 
@@ -98,16 +99,59 @@ class KMeansClustering:
             return
         self.df = pd.read_excel(self.path)
 
-    def update(self, method):
-        if method == "add":
-            self.total += self.entered_number
-        elif method == "subtract":
-            self.total -= self.entered_number
-        else:  # reset
-            self.total = 0
+    def pre_process(self):  # todo: need maybe try - except???
+        # pre process the df
+        preP = PreProcess(self.df)
+        self.df = preP.df
+        self.is_processed = True
+        tkMessageBox.showinfo("K Means Clustering", "Preprocessing completed successfully!")
+        pass
 
-        self.total_label_text.set(self.total)
-        self.path_entry.delete(0, END)
+    def k_means(self):
+        if not self.is_processed:
+            tkMessageBox.showerror("K Means Clustering", "You need to pre-process the data before clustering")
+            return
+        # check values of runs and cluster numbers
+        try:
+            n_runs = self.runs_entry.get()
+            n_clusters = self.cluster_entry.get()
+            if n_runs == "" or n_clusters == "":
+                tkMessageBox.showerror("K Means Clustering", "You have to insert numbers")
+                return
+            n_clusters = int(n_clusters)
+            n_runs = int(n_runs)
+            if n_clusters <= 0:
+                tkMessageBox.showerror("K Means Clustering", "Number of clusters must be positive")
+                return
+
+            if n_runs <= 0:
+                tkMessageBox.showerror("K Means Clustering", "Number of runs must be positive")
+                return
+            if n_clusters > len(self.df):
+                tkMessageBox.showerror("K Means Clustering", "The number of clusters is too big")
+                return
+
+
+        except Exception:
+            tkMessageBox.showerror("K Means Clustering", "Invalid numbers")
+            return
+
+        # create cluster object
+        k_means = Cluster(self.df, n_clusters, n_runs)
+        self.df = k_means.df
+
+        # create label for scatter image and layout
+        scatter_img = ImageTk.PhotoImage(Image.open('scatter.png'))
+        self.scatter_label = Label(self.master, image=scatter_img)
+        self.scatter_label.image = scatter_img
+        self.scatter_label.grid(row=6, column=0, columnspan=10)
+
+        horopleth_img = ImageTk.PhotoImage(Image.open('horopleth.png'))
+        self.horopleth_label = Label(self.master, image=horopleth_img)
+        self.horopleth_label.image = horopleth_img
+        self.horopleth_label.grid(row=6, column=10, columnspan=10)
+
+        tkMessageBox.showinfo("K Means Clustering", "Clustering completed successfully!")
 
 root = Tk()
 my_gui = KMeansClustering(root)
